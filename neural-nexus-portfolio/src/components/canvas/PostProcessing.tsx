36cb64aed9c6d7ad9e5b76f5aedf7019d95971f0
sleepyMS <minseok011017@gmail.com>
2025-12-14 03:14:41 +0900

    ref: 라이트모드 광원 제거

--- neural-nexus-portfolio/src/components/canvas/PostProcessing.tsx
+++ neural-nexus-portfolio/src/components/canvas/PostProcessing.tsx
@@ -1,22 +1,27 @@
 import { EffectComposer, Bloom, Vignette } from "@react-three/postprocessing";
+import { useAppStore } from "../../stores/useAppStore";
 
 /**
  * 후처리 효과 컴포넌트
  * Bloom과 Vignette로 시네마틱한 느낌 연출
+ * 라이트 모드에서는 Bloom 강도를 줄여 과도한 밝기 방지
  */
 export function PostProcessing() {
+  const { theme } = useAppStore();
+  const isDark = theme === "dark";
+
   return (
     <EffectComposer>
-      {/* 노드 빛남을 위한 Bloom 효과 */}
+      {/* 노드 빛남을 위한 Bloom 효과 - 라이트 모드에서는 약하게 */}
       <Bloom
-        intensity={1.5}
-        luminanceThreshold={0.2}
+        intensity={isDark ? 1.5 : 0.3}
+        luminanceThreshold={isDark ? 0.2 : 0.6}
         luminanceSmoothing={0.9}
         mipmapBlur
       />
 
-      {/* 시네마틱한 느낌을 위한 비네트 효과 */}
-      <Vignette offset={0.3} darkness={0.6} />
+      {/* 시네마틱한 느낌을 위한 비네트 효과 - 라이트 모드에서는 약하게 */}
+      <Vignette offset={0.3} darkness={isDark ? 0.6 : 0.2} />
     </EffectComposer>
   );
 }
