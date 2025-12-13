5cbe24129afe88c137839835353d4ddaaee6f09a
sleepyMS <minseok011017@gmail.com>
2025-12-14 03:55:23 +0900

    feat: 달 자전/공전 주기 동기화
    - 실제 달은 조석 고정 상태로, 자전과 공전 주기가 같음

--- neural-nexus-portfolio/src/components/canvas/Background.tsx
+++ neural-nexus-portfolio/src/components/canvas/Background.tsx
@@ -16,6 +16,10 @@ export function Background() {
   const sunRef = useRef<THREE.Mesh>(null);
   const moonRef = useRef<THREE.Mesh>(null);
 
+  // 공전 궤도용 ref
+  const moonOrbitRef = useRef<THREE.Group>(null);
+  const sunOrbitRef = useRef<THREE.Group>(null);
+
   // 달 텍스처 로드 (로컬 이미지)
   const moonTexture = useTexture("/images/moon_texture.jpg");
 
@@ -71,13 +75,25 @@ export function Background() {
       colors.needsUpdate = true;
     }
 
-    // 태양 천천히 회전
+    // 태양 자전 + 공전
     if (sunRef.current) {
       sunRef.current.rotation.z += delta * 0.1;
     }
-    // 달 천천히 회전
+    if (sunOrbitRef.current) {
+      // 태양 공전 (60초에 1바퀴)
+      sunOrbitRef.current.rotation.y += delta * 0.02;
+    }
+
+    // 달 자전 + 공전 (조석 고정: 자전 = 공전 속도)
+    // 달은 항상 같은 면이 보이도록 자전과 공전 속도가 같음
+    const moonOrbitSpeed = delta * 0.015;
     if (moonRef.current) {
-      moonRef.current.rotation.y += delta * 0.05;
+      // 자전 속도 = 공전 속도 (Tidal Locking)
+      moonRef.current.rotation.y += moonOrbitSpeed;
+    }
+    if (moonOrbitRef.current) {
+      // 달 공전 (90초에 1바퀴)
+      moonOrbitRef.current.rotation.y += moonOrbitSpeed;
     }
   });
 
@@ -95,50 +111,56 @@ export function Background() {
             fade
             speed={0.5}
           />
-          {/* 달 */}
-          <group position={[-60, 40, -150]}>
-            {/* 달빛 광원 - 달 뒤쪽에서 비추기 */}
-            <pointLight
-              position={[0, 0, -20]}
-              color="#b0c4de"
-              intensity={30}
-              distance={300}
-              decay={2}
-            />
-            <mesh ref={moonRef} rotation={[0, 0, Math.PI / 8]}>
-              <sphereGeometry args={[12, 64, 64]} />
-              <meshStandardMaterial
-                map={moonTexture}
-                emissive="#aabbcc"
-                emissiveIntensity={1.8}
-                emissiveMap={moonTexture}
-                roughness={0.95}
-                metalness={0}
+          {/* 달 공전 궤도 */}
+          <group ref={moonOrbitRef}>
+            {/* 달 - 궤도 반경 170, 높이 40 */}
+            <group position={[-120, 30, -120]}>
+              {/* 달빛 광원 - 달 뒤쪽에서 비추기 */}
+              <pointLight
+                position={[0, 0, -20]}
+                color="#b0c4de"
+                intensity={30}
+                distance={300}
+                decay={2}
               />
-            </mesh>
+              <mesh ref={moonRef} rotation={[0, 0, Math.PI / 8]}>
+                <sphereGeometry args={[12, 64, 64]} />
+                <meshStandardMaterial
+                  map={moonTexture}
+                  emissive="#aabbcc"
+                  emissiveIntensity={1.8}
+                  emissiveMap={moonTexture}
+                  roughness={0.95}
+                  metalness={0}
+                />
+              </mesh>
+            </group>
           </group>
         </>
       )}
 
       {/* 라이트 모드: 태양 */}
       {!isDark && (
-        <group position={[60, 40, -150]}>
-          {/* 태양빛 광원 - 따뜻한 노란 빛 */}
-          <pointLight
-            color="#fff5e0"
-            intensity={100}
-            distance={400}
-            decay={2}
-          />
-          <mesh ref={sunRef}>
-            <sphereGeometry args={[16, 32, 32]} />
-            <meshBasicMaterial color="#ffd93d" />
-            {/* 태양 글로우 */}
-            <mesh>
-              <sphereGeometry args={[24, 32, 32]} />
-              <meshBasicMaterial color="#fff3b0" transparent opacity={0.3} />
+        <group ref={sunOrbitRef}>
+          {/* 태양 - 궤도 반경 170, 높이 40 */}
+          <group position={[120, 30, -120]}>
+            {/* 태양빛 광원 - 따뜻한 노란 빛 */}
+            <pointLight
+              color="#fff5e0"
+              intensity={100}
+              distance={400}
+              decay={2}
+            />
+            <mesh ref={sunRef}>
+              <sphereGeometry args={[16, 32, 32]} />
+              <meshBasicMaterial color="#ffd93d" />
+              {/* 태양 글로우 */}
+              <mesh>
+                <sphereGeometry args={[24, 32, 32]} />
+                <meshBasicMaterial color="#fff3b0" transparent opacity={0.3} />
+              </mesh>
             </mesh>
-          </mesh>
+          </group>
         </group>
       )}
 
